b68cef6b075b10db56c0f8dd0729b7336be9440c
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-02-07 12:19:25 +1100

    Feat: initial commit of tippy.js adder

---
+++ tooltip.js
@@ -0,0 +1,10 @@
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+// Select all elements that contain an aristotle id
+let elements = document.querySelectorAll('[data-aristotle-id]');
+
+// Add an tippy for each element that has an attached aristotle id
+for (let element in elements) {
+
+}
