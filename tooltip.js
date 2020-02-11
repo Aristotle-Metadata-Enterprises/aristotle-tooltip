078e03ddfba03312dc0a1b688cf8e1a5c850815b
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-02-11 12:30:52 +1100

    Refactor: refactor request function

--- tooltip.js
+++ tooltip.js
@@ -7,82 +7,70 @@ import 'tippy.js/themes/translucent.css';
 
 import axios from 'axios';
 
-let baseUrl = null;
-
-function processRequest(aristotleId, baseUrl, tippyInstance) {
+function makeRequest(aristotleId, baseUrl) {
     let url = `${baseUrl}/api/v4/item/${aristotleId}/`;
+    let item = {};
+
     axios.get(url)
         .then((response) => {
-            tippyInstance.name = response.data['name']
-            tippyInstance.definition = response.data['definition']
-            tippyInstance.shortDefinition = response.data['short_definition']
-            tippyInstance.aristotleId = aristotleId
-
-            tippyInstance._permanent_failure = false;
-            tippyInstance._isFetching = false;
-            tippyInstance._isLoadedSuccessfully = true;
-
-            let contentWithHtml = addHtmlComponents(tippyInstance)
-            tippyInstance.setContent(contentWithHtml);
+            item.name = response.data['name']
+            item.shortDefinition = response.data['short_definition']
 
         }).catch((error) => {
-            if (error.response) {
-                //  The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                let status_code = error.response.status;
+        if (error.response) {
+            //  The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            let status_code = error.response.status;
 
-                if (status_code === 401 || status_code === 403) {
-                    tippyInstance.setContent("ERROR: This item is not publicly viewable");
-                    tippyInstance._permanent_failure = true;
-                } else if (String(status_code).startsWith('5')) {
-                    // It's a 500 failure
-                    tippyInstance.setContent("ERROR: The server is currently experiencing errors. Please try again later.");
-                    tippyInstance._permanent_failure = false;
-                } else {
-                    // Any other failure
-                    tippyInstance.setContent("ERROR: The server cannot process your request. Please try again later.");
-                    tippyInstance._permanent_failure = false;
-                }
-            } else if (error.request) {
-                // The request was made but no response was received
-                tippyInstance.setContent("ERROR: No response was received from the server. Please try again later");
-                tippyInstance._permanent_failure = false;
+            if (status_code === 401 || status_code === 403) {
+                item.shortDefinition("ERROR: This item is not publicly viewable");
+                item._permanent_failure = true;
+            } else if (String(status_code).startsWith('5')) {
+                // It's a 500 failure
+                item.shortDefinition("ERROR: The server is currently experiencing errors. Please try again later.");
+                item._permanent_failure = false;
+            } else {
+                // Any other failure
+                item.shortDefinition("ERROR: The server cannot process your request. Please try again later.");
+                item._permanent_failure = false;
             }
-        });
-}
-
-// Select all elements that contain an aristotle id
-let elements = document.querySelectorAll('[data-aristotle-id]');
-
-// Create a Tippy object for each element that has an attached aristotle id:
-for (let element of elements) {
-    let aristotleId = element.dataset.aristotleId;
-    tippy(element, {
-        content: 'Loading... <button id="my-test-button">hello world</button>',
-        flipOnUpdate: true, // Because the tooltip changes sizes when the definition successfully loads
-        interactive: true,
-        theme: 'light-border',
-        onCreate(instance) {
-            // Keep track of state
-            instance._isFetching = false;
-            instance._permanent_failure = null;
-            instance._content = null;
-            instance._isLoadedSuccessfully = false;
-        },
-        onShow(instance) {
-            if (instance._isLoadedSuccessfully || instance._isFetching || instance._permanent_failure) {
-                return;
-            }
-            instance._isFetching = true;
-
-            processRequest(aristotleId, 'https://registry.aristotlemetadata.com', instance)  // TODO: Change this later.
-
+        } else if (error.request) {
+            // The request was made but no response was received
+            item.shortDefinition("ERROR: No response was received from the server. Please try again later");
+            item._permanent_failure = false;
         }
     });
+    return item;
 }
 
-function truncateByWords(content, numberOfWords) {
-    return content.split(" ").splice(0, numberOfWords).join(" ");
+
+function createTippyElements(baseURL) {
+    // Select all elements that contain an aristotle id
+    let elements = document.querySelectorAll('[data-aristotle-id]');
+
+    // Create a Tippy object for each element that has an attached aristotle id:
+    for (let element of elements) {
+        let aristotleId = element.dataset.aristotleId;
+        tippy(element, {
+            content: 'Loading...',
+            flipOnUpdate: true, // Because the tooltip changes sizes when the definition successfully loads
+            interactive: true,
+            theme: 'light-border',
+            onCreate(instance) {
+                // Keep track of state
+                instance._isFetching = false;
+                instance._permanent_failure = null;
+            },
+            onShow(instance) {
+                if (instance._isFetching || instance._permanent_failure) {
+                    return;
+                }
+                instance._isFetching = true;
+                let content = makeRequest(aristotleId, baseURL);
+                console.log(content);
+            }
+        });
+    }
 }
 
 function addHtmlComponents(tippyInstance) {
@@ -101,10 +89,5 @@ function addHtmlComponents(tippyInstance) {
 
     return documentFragment.outerHTML
 }
-document.addEventListener('click',function(e){
-    if(e.target && e.target.id == 'my-test'){
-        let instance = e.target.parentElement.parentElement.parentElement.parentElement._tippy
-        instance.setContent("Hello world")
-        //do something
-    }
-});
+
+createTippyElements('https://registry.aristotlemetadata.com');
