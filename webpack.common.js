7e982749ea1519b8b46752680eafb5a82a8256b9
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-02-14 12:21:07 +1100

    Fix: fixed URL loader

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: {
+        app: './index.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Production',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 25000,
+                        },
+                    },
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: "aristotle-tooltip.js",
+        library: "aristotle-tooltip",
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
