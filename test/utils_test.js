5412dbb7bc5f9da95b7dd1f1c0691f7a76c337ae
Dylan Silke <d.ylan.s.silke@gmail.com>
2020-02-19 10:39:26 +1100

    Test: added strip html test

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,15 +1,24 @@
 import chai from 'chai'
 const assert = chai.assert;
 
-import {getItemLink} from "../src/utils.js"
+import {getItemLink, stripHtmlTags} from "../src/utils.js"
 
 describe('getItemLink', function () {
 
-    it('Returns a item link when attrs defined', function () {
+    it('should return a item link when attrs defined', function () {
         let baseUrl = 'https://registry.aristotlemetadata.com';
         let aristotleId = 123123;
 
         let result = getItemLink(baseUrl, aristotleId);
         assert.equal('https://registry.aristotlemetadata.com/item/123123/', result);
+    });
+});
+
+describe('stripHtmlTags', function () {
+    it('should strip simple html tags', function () {
+        let htmlString = '<p>This is a very interesting paragraph</p>';
+        let strippedHtml = stripHtmlTags(htmlString);
+
+        assert.equal('This is a very interesting paragraph', strippedHtml);
     });
 });
