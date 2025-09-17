b7250525742ed454d536785fbd6be3d4bcec900a
harry silke <harry.i.silke@gmail.com>
2025-09-17 14:04:08 +1000

    task: bring back security

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function (config) {
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
                 base: 'ChromeHeadless',
-                flags: ['--no-sandbox', '--disable-web-security']
+                flags: ['--no-sandbox'],
             }
         },
         reporters: ['mocha'],
